50043ed1712cb83eff72b2a3119ddcc6e26ffb66
Your Name <you@example.com>
2022-02-19 17:06:19 +1100

    wrote something

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { AiFillPlayCircle } from 'react-icons/ai';
+
+const companyCommonStyles =
+  'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
 const Welcome = () => {
   return (
@@ -7,6 +11,35 @@ const Welcome = () => {
         <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
           Send Crypto <br /> across the world
         </h1>
+      </div>
+      {true && (
+        <button
+          type="button"
+          onClick={() => {
+            console.log('debug here ');
+          }}
+          className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+        >
+          <AiFillPlayCircle className="text-white mr-2" />
+          <p className="text-white text-base font-semibold">Connect Wallet</p>
+        </button>
+      )}
+
+      <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+        <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+          Reliability
+        </div>
+        <div className={companyCommonStyles}>Security</div>
+        <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
+          Ethereum
+        </div>
+        <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
+          Web 3.0
+        </div>
+        <div className={companyCommonStyles}>Low Fees</div>
+        <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+          Blockchain
+        </div>
       </div>
     </div>
   );
---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,33 @@
+const { run } = require('hardhat');
+const hre = require('hardhat');
+
+const main = async () => {
+  // We get the contract to deploy
+  const Transaction = await hre.ethers.getContractFactory('Transaction');
+  const transaction = await Transaction.deploy('Hello, Hardhat!');
+
+  await transaction.deployed();
+
+  console.log('Transaction deployed to:', transaction.address);
+};
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+runMain();
